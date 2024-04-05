eb98d8e7115c28929dbc06ac5f36b01bce6228c6
SebastianV91 <severgara91@hotmail.com>
2024-04-05 02:17:18 -0500

    CREACION PLANTILLA LOGIN

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Register from "./components/Register";
+import Login from "./components/Login";
+
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-        <Register/>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/home" element= { <Home/>} />
+              <Route path="/register" element= { <Register/>} />
+              <Route path="/" element= { <Login/>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
